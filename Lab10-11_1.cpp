f261c00932474638cd96058c30e10656aa0a9650
evgen-2007 <mamontovevgen2007@gmail.com>
2024-11-15 10:46:50 +0200

    Add files via upload

---
+++ Lab10-11_1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cstdio>
+
+int a;
+int b;
+int c;
+
+int main() {
+	a = 1;
+	b = a + 3;
+	c = a + b;
+	printf("a=  %d  b= %d   c= %d  \n", a, b, c);
+	return 0;
+}
---
+++ Lab10-11_2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <math.h>
+float f;
+float a;
+float b;
+float c;
+float fi;
+float o;
+float omega;
+float M_PI;
+
+
+float main() {
+	printf("vvedit a \n");
+	scanf_s("%f", &a);
+	printf("vvedit b \n");
+	scanf_s("%f", &b);
+	printf("vvedit c \n");
+	scanf_s("%f", &c);
+	printf("vvedit φ \n");
+	scanf_s("%f", &fi);
+	printf("vvedit ω \n");
+	scanf_s("%f", &omega);
+	printf("vvedit θ \n");
+	scanf_s("%f", &o);
+	M_PI = 3.14;
+	f = pow(a * c + 8 * c. / b., 1. /5.) - 1 / tan(a * fi - o) - 45 * M_PI - 7. / omega.;
+	printf("f= %f \n", f);
+	return 0;
+}
